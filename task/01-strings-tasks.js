ecd40d0a5d689889a1d04b53abe998a606688851
Nikita Plakhin <nplkhn1@yandex.ru>
2019-04-13 17:26:41 +0300

    string done

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -228,6 +228,9 @@ function getRectangleString(width, height) {
  *
  */
 function encodeToRot13(str) {
+    return str.replace(/[a-zA-Z]/g,function(c){
+        return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);
+    });
     throw new Error('Not implemented');
 }
 
